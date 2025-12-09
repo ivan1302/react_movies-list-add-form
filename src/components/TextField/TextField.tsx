9628af0e3307d5c2eed50449d07e9f828b3b69dc
Ivan Shcherbatiuk <shcherbatyuk13@gmail.com>
2025-12-09 19:40:47 +0200

    add task solution4

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -7,7 +7,7 @@ type Props = {
   label?: string;
   placeholder?: string;
   required?: boolean;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 function getRandomDigits() {
